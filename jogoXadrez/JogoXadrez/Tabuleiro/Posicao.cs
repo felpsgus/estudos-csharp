1e367833027ee687f63841d17366a6ade281409d
Felipe Augusto Lopes Veras <felipeaugustolopesveras@gmail.com>
2024-01-25 22:34:46 -0300

    criação da classe de posição

---
+++ jogoXadrez/JogoXadrez/Tabuleiro/Posicao.cs
@@ -0,0 +1,13 @@
+namespace JogoXadrez.Tabuleiro;
+
+public class Posicao
+{
+    public int Linha { get; set; }
+    public int Coluna { get; set; }
+
+    public Posicao(int linha, int coluna)
+    {
+        Linha = linha;
+        Coluna = coluna;
+    }
+}
