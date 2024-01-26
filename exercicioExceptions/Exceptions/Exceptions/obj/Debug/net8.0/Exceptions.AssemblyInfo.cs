c088415718f1db5ba05bf5ad72587c95e5130c0e
Felipe Augusto Lopes Veras <felipeaugustolopesveras@gmail.com>
2024-01-25 22:24:17 -0300

    melhoria no gitignore

--- exercicioExceptions/Exceptions/Exceptions/obj/Debug/net8.0/Exceptions.AssemblyInfo.cs
+++ exercicioExceptions/Exceptions/Exceptions/obj/Debug/net8.0/Exceptions.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exceptions")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39f1453ecd6d207c8c8f219ab6bb7ecdd4d70bd7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4cd1bac964a70a5c0bbb4f99736e6e40a55e067")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exceptions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exceptions")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
